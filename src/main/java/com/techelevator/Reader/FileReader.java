5a9c3e57ffabfd6b9d59f571d0b45f7f4f2252ed
Katie Paulsen <paulsennkatie@gmail.com>
2022-07-28 15:13:09 -0400

    capstone third push

--- src/main/java/com/techelevator/Reader/FileReader.java
+++ src/main/java/com/techelevator/Reader/FileReader.java
@@ -1,12 +1,19 @@
 package com.techelevator.Reader;
 
+import com.techelevator.inventory.Candy;
+import com.techelevator.inventory.Drink;
+import com.techelevator.inventory.Gum;
+import com.techelevator.inventory.Munchy;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class FileReader {
 
     public void readFile(){
+
         String pathOfFileToRead = "C:\\Users\\Student\\workspace\\module-1-capstone-team-7\\catering.csv";
 
         //create file object
@@ -14,8 +21,29 @@ public class FileReader {
 
         //Create new scanner
         try(Scanner fileReader = new Scanner(inventoryFile)){
+
             while(fileReader.hasNextLine()) {
-                System.out.println(fileReader.nextLine());
+
+                String currentLine = fileReader.nextLine();
+                String[] delimitedString = currentLine.split(",");
+
+                String slotLocation = delimitedString[0];
+                String name = delimitedString[1];
+                String price = delimitedString[2];
+                String itemType = delimitedString[3];
+
+                if(itemType.equals("Candy")){
+                    Candy candy = new Candy(slotLocation);
+                } else if(itemType.equals("Gum")){
+                    Gum gum = new Gum(slotLocation);
+                } else if(itemType.equals("Drink")){
+                    Drink drink = new Drink(slotLocation);
+                } else if(itemType.equals("Munchy")){
+                    Munchy munchy = new Munchy(slotLocation);
+                }
+
+                System.out.println(currentLine);
+                System.out.println(delimitedString[1]);
             }
 
         }catch(FileNotFoundException e) {
--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,11 +1,15 @@
 package com.techelevator;
 
 
+import com.techelevator.Reader.FileReader;
 import com.techelevator.application.VendingMachine;
 
 public class VendingMachineCLI {
 
 	public static void main(String[] args) {
+
+		FileReader fileReader = new FileReader();
+		fileReader.readFile();
 
 		VendingMachine vendingMachine = new VendingMachine();
 		vendingMachine.run();
